894f7538827dd161fb3f5d3587971ab9189bf194
ozaki <bitter.life.club@gmail.com>
2026-03-27 14:36:15 +0900

    List格納用の値のクラスを追加

--- script/value.go
+++ script/value.go
@@ -1,21 +1,38 @@
 package script
 
+// 値の型定義
 type ValueType int
 
 const (
-	TypeNil ValueType = iota
-	TypeNumber
+	TypeNumber ValueType = iota
 	TypeBool
-	TypeStr
-	TypeSymbol
-	TypeProperty
-	TypeList // AST（ツリー構造）の時のみ使用
+	TypeString
+	TypeProperty // @i などの参照（未評価）
+	TypeList     // (op arg1 arg2)
+
+	// 制御構造の型
+	TypeSwitch // switch文
+	TypeRepeat // repeat文
 )
 
+// Listに格納する型付きの値。リフレクションを避けるため全部入り
 type Value struct {
-	Type   ValueType
-	Number float64
-	Bool   bool
-	Str    string
-	List   []Value // 子要素（S式の入れ子）
+	// 型情報
+	Type ValueType
+
+	// 基本データ
+	Num  float64
+	Bool bool
+	Str  string
+	Prop string
+
+	// リスト構造 (S式や、分解済みの文字列フラグメント)
+	List []Value
 }
+
+// 値の生成関数
+func NewNumber(f float64) Value  { return Value{Type: TypeNumber, Num: f} }
+func NewBool(b bool) Value       { return Value{Type: TypeBool, Bool: b} }
+func NewString(s string) Value   { return Value{Type: TypeString, Str: s} }
+func NewProperty(v string) Value { return Value{Type: TypeProperty, Prop: v} }
+func NewList(v []Value) Value    { return Value{Type: TypeList, List: v} }
