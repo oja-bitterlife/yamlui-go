d1dd2481723c780a209955380ad13dbb77c2ec62
ozaki <bitter.life.club@gmail.com>
2026-03-28 18:56:19 +0900

    strconvを使うようにした

--- script/util.go
+++ script/util.go
@@ -1,26 +1,13 @@
 package script
 
-import "errors"
-
-func Itoa(n int) string {
-	if n == 0 {
-		return "0"
-	}
-	buf := make([]byte, 0, 10)
-	for n > 0 {
-		buf = append(buf, byte('0'+(n%10)))
-		n /= 10
-	}
-	// 反転処理
-	for i, j := 0, len(buf)-1; i < j; i, j = i+1, j-1 {
-		buf[i], buf[j] = buf[j], buf[i]
-	}
-	return string(buf)
-}
+import (
+	"errors"
+	"strconv"
+)
 
 func ValidationArgNum(cmdName string, argNum int, args []Value) error {
 	if len(args) != argNum {
-		argNumStr := Itoa(argNum)
+		argNumStr := strconv.Itoa(argNum)
 		return errors.New(cmdName + ": requires exactly " + argNumStr + " arguments")
 	}
 	return nil
