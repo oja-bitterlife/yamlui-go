dc5bb9dfc6ddf6f45d6d170aa661aba408ef8718
ozaki <bitter.life.club@gmail.com>
2026-04-05 15:47:25 +0900

    コメント追加

--- script/log_release.go
+++ script/log_release.go
@@ -7,9 +7,11 @@ import (
 	"io"
 )
 
+// 完全に消えることを期待
+// ==================================================
 func SetLogWriter(w io.Writer) {}
 
-// 完全に消えることを期待
+// ----------------------------------------
 func Log(msg string, args ...any) {}
 func LogErr(msg string, args ...any) error {
 	return errors.New(msg)
